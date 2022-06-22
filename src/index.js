71f7eb4f69ec1f3afb1b26320ddb9e72f537c114
enzopcastillo <epcastillov@gmail.com>
2022-06-22 14:51:37 -0300

    Completando index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-console.log('hello');
+require('./database');
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+app.use(cors({origin: "http://localhost:4200"}));
+app.use(express.json());
+
+//app.use('/api/agente', require('./routes/agente.route'));
+
+app.set('port', process.env.PORT || 4000);
+app.listen(app.get('port'), ()=>{
+    console.log('Server started on port', app.get('port'));
+});
