8d454eb062f887b3fa0d722caba8e87fbba0c545
enzopcastillo <epcastillov@gmail.com>
2022-06-22 15:28:19 -0300

    Finalizando database.js

---
+++ src/database.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+const url_database = 'mongodb://localhost/final_database';
+
+mongoose.connect(url_database, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+})
+.then(db => console.log('Database connected'))
+.catch(err => console.error(err));
