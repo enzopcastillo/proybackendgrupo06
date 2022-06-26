4486443ac5a35c878920a6160b2f8c795a07dea8
enzopcastillo <epcastillov@gmail.com>
2022-06-25 21:42:33 -0300

    Testing

--- src/index.js
+++ src/index.js
@@ -7,8 +7,6 @@ app.use(cors({origin: "http://localhost:4200"}));
 app.use(express.json());
 
 app.use('/api/empleados', require('./routes/empleado.routes'));
-app.use('/api/administradores', require('./routes/administrador.routes'));
-app.use('/api/participantes', require('./routes/participante.routes'));
 
 app.set('port', process.env.PORT || 4000);
 app.listen(app.get('port'), ()=>{
