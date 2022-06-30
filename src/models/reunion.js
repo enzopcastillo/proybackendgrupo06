50d03285c54fc0ad4635e3ed8bc98dd137af4f9a
enzopcastillo <epcastillov@gmail.com>
2022-06-30 16:28:04 -0300

    Añadiendo atributos faltantes a la clase reunion

--- src/models/reunion.js
+++ src/models/reunion.js
@@ -3,8 +3,11 @@ const TipoReunion = require('./tipoReunion');
 const Oficina = require('./oficina');
 const Empleado = require('./empleado');
 const Recurso = require('./recurso');
+const Notificacion = require('./notificacion');
 
 const reunionSchema = new Schema({
+    nombre: {type: String, required: true},
+    descripcion: {type: String, required: true},
     fecha: {type: String, required: true},
     horaReunion: {type: String, required: true},
     horaFinalizacion: {type: String, required: true},
@@ -15,6 +18,7 @@ const reunionSchema = new Schema({
     recursos: [{type: Schema.Types.ObjectId, ref: Recurso, required: true}],
     prioridad: {type: Number, required: true},
     codigoQr: {type: String, required: true},
+    notificacion: [{type: Schema.Types.ObjectId, ref: Notificacion, required: true}],
 })
 
 module.exports = model('Reunion', reunionSchema);
