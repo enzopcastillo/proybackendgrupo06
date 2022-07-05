06cdfe0657a52361c054b33a1ed979f3b0973e99
SofiaGDelgado <sofigdel@gmail.com>
2022-07-05 14:25:53 -0600

    Cambios en getReunionesParticipantes

--- src/routes/empleado.routes.js
+++ src/routes/empleado.routes.js
@@ -3,11 +3,11 @@ const router = Router();
 const empleadoCtl = require('../controllers/empleado.controllers');
 const authCtl = require('../controllers/auth.controllers');
 
-router.post('/', empleadoCtl.createEmpleado); // authCtl.verifyToken,
-router.post('/login', empleadoCtl.loginEmpleado);
-router.get('/',  empleadoCtl.getEmpleados);
-router.get('/:id', empleadoCtl.getEmpleado);
-router.put('/:id',  empleadoCtl.updateEmpleado);
-router.delete('/:id', empleadoCtl.deleteEmpleado);
+router.post('/', authCtl.verifyToken, empleadoCtl.createEmpleado); // authCtl.verifyToken,
+router.post('/login',authCtl.verifyToken, empleadoCtl.loginEmpleado);
+router.get('/', authCtl.verifyToken, empleadoCtl.getEmpleados);
+router.get('/:id',authCtl.verifyToken, empleadoCtl.getEmpleado);
+router.put('/:id', authCtl.verifyToken, empleadoCtl.updateEmpleado);
+router.delete('/:id',authCtl.verifyToken, empleadoCtl.deleteEmpleado);
 
 module.exports = router;
